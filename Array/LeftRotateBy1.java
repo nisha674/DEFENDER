e5064d4e4e52339087784eed23ad726919e256b3
Nisha Ingole <ingolenisha9@gmail.com>
2026-06-09 20:46:40 +0530

    Day 4: Arrays

---
+++ Array/LeftRotateBy1.java
@@ -0,0 +1,19 @@
+package Array;
+
+class LeftRotateBy1{
+    public static void main(String[] args){
+        int[] arr = {1 , 2 , 3 , 4, 5 };
+
+        int temp = arr[0];
+
+        for(int i = 0; i < arr.length-1; i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[arr.length-1] = temp;
+
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
